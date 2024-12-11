ffca962d495b77a5dd4b5266cb3cc1dc4963b519
blatantBabbage <ransinghbel@gmail.com>
2024-12-12 00:38:28 +0530

    Find missing number using XOR

---
+++ arrayProblems/easy/missingNumber.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Singh, Nitesh on 11/12/24.
+//
+#include<iostream>
+using namespace std;
+
+/*
+ *  Problem statement
+    Given an array ‘a’ of size ‘n’-1 with elements of range 1 to ‘n’. The array does not contain any duplicates.
+    Your task is to find the missing number.
+
+    For example:
+    Input:
+    'a' = [1, 2, 4, 5], 'n' = 5
+
+    Output : 3
+ */
+
+// Using optimal XOR approach
+int missingNumber(int a[], int n)
+{
+
+    // Initialize XOR1, to n natural number
+    int xor1 = 0;
+    for(int i = 1; i<n; i++)
+    {
+        xor1 = xor1^i;
+    }
+
+    // Initialize XOR2, by looping on array which missing number
+    int xor2 = 0;
+    for(int i = 0; i<n-1; i++)
+    {
+        xor2 = xor2^a[i];
+    }
+
+    return xor1^xor2;
+}
